191066a1fe734caa4b5fa9ea05e71fd25c6fb1fe
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-04-13 19:35:03 +0300

    edit polybar/polybar-scripts/updates.sh

--- polybar/polybar-scripts/updates.sh
+++ polybar/polybar-scripts/updates.sh
@@ -4,7 +4,7 @@ updates() {
 
         list_upgradeable=$(apt list --upgradeable -qq | cut -d ' ' -f 2 | wc -l)
         if [ "$list_upgradeable" -gt "0" ]; then
-            echo -e "[u]" && dunstify '[u]' 'Update Available' -A ~/.config/polybar/polybar-scripts/updates.sh --upgrade >/dev/null
+            echo -e "[u]"
         else
             echo  ""
         fi
