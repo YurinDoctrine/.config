3dd02eaca685440c925ddfdd6ae148c104cc4e92
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-04-13 15:04:37 +0300

    added updates/upgrades

---
+++ polybar/polybar-scripts/updates.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/sh
+
+updates() {
+
+        list_upgradeable=$(apt list --upgradeable -qq | cut -d ' ' -f 2 | wc -l)
+        if [ "$list_upgradeable" -gt "0" ]; then
+            echo -e "[U]" && dunstify 'Updates Available' >/dev/null
+        else
+            echo  ""
+        fi
+}
+
+upgrade() {
+    cbpp-gksudo xterm -e "apt-get dist-upgrade -yy"
+}
+
+case "$1" in
+    --upgrade)
+        upgrade
+        ;;
+    *)
+        updates
+        ;;
+esac
