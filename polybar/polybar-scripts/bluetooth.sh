71ec8d404262d2b06899cd80415d97053280cc91
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-04-14 10:52:04 +0300

    update polybar

--- polybar/polybar-scripts/bluetooth.sh
+++ polybar/polybar-scripts/bluetooth.sh
@@ -1,36 +1,26 @@
 #!/usr/bin/sh
 
 bluetooth_print() {
-    bluetoothctl | while read -r; do
-        if [ "$(systemctl is-active "bluetooth.service")" = "active" ]; then
-            #printf '#1'
-            echo ""
+    if [ "$(lsusb | grep Bluetooth)" ]; then
+        bluetoothctl | while read -r; do
+            if [ "$(systemctl is-active "bluetooth.service")" = "active" ]; then
 
-            devices_paired=$(bluetoothctl paired-devices | grep Device | cut -d ' ' -f 2)
-            counter=0
+                echo ""
+                devices_paired=$(bluetoothctl paired-devices | grep Device | cut -d ' ' -f 2)
+                echo "$devices_paired" | while read -r line; do
 
-            echo "$devices_paired" | while read -r line; do
-                device_info=$(bluetoothctl info "$line")
-
-                if echo "$device_info" | grep -q "Connected: yes"; then
-                    #device_alias=$(echo "$device_info" | grep "Alias" | cut -d ' ' -f 2-)
-
-                    #if [ $counter -gt 0 ]; then
-                        #printf ", %s" "$device_alias"
-                    #else
-                        #printf " %s" "$device_alias"
-                    #fi
-
-                    #counter=$((counter + 1))
-                    echo ""
-                fi
-            done
-
-            #printf '\n'
-        else
-            echo  ""
-        fi
-    done
+                    device_info=$(bluetoothctl info "$line")
+                    if echo "$device_info" | grep -q "Connected: yes"; then
+                        echo ""
+                    fi
+                done
+            else
+                echo  ""
+            fi
+        done
+    else
+        exit
+    fi
 }
 
 bluetooth_toggle() {
