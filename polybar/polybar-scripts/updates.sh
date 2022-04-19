57c400597768e4292f7a38d705b0ffba5b90ff4c
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-04-19 11:21:51 +0300

    edit polybar/polybar-scripts/updates.sh

--- polybar/polybar-scripts/updates.sh
+++ polybar/polybar-scripts/updates.sh
@@ -11,7 +11,7 @@ updates() {
 }
 
 upgrade() {
-    DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical cbpp-gksudo xterm -e 'apt-get -yy dist-upgrade' 2>&1 && echo "" >/dev/null
+    DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical cbpp-gksudo xterm -e 'apt-get -yy dist-upgrade' 2>&1 >/dev/null && echo ""
 }
 
 case "$1" in
