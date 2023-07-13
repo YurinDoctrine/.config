b982f04d3505d8a153099e057f77ed8998a3b7c7
YurinDoctrine <bayramyurgidenn@gmail.com>
2023-07-13 10:46:15 +0300

    edit polybar/config.ini , polybar/polybar-scripts/activewindow.sh

--- polybar/polybar-scripts/activewindow.sh
+++ polybar/polybar-scripts/activewindow.sh
@@ -4,9 +4,16 @@ get_window() {
   xdotool windowactivate "$(xdotool getactivewindow)" >> /dev/null ;
 }
 
+kill_task() {
+  kill "$(xdotool getwindowfocus getwindowpid)" >> /dev/null ;
+}
+
 case "$1" in
     --get)
         get_window
+        ;;
+    --kill)
+        kill_task
         ;;
     *)
         xdotool getwindowfocus getwindowname ;
