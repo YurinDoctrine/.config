5c50d6fbdd473167425744fde689c06a374e6d13
Yurin Doctrine <doctriney@protonmail.com>
2023-10-27 07:03:57 +0000

    edit polybar/polybar-scripts/macchanger.sh

--- polybar/polybar-scripts/macchanger.sh
+++ polybar/polybar-scripts/macchanger.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
 if [[ $(ip link | grep 'UP mode' | wc -l) -gt 0 ]]; then
-	ip link | awk '/UP mode/ {print $2}' | tr -d ':' | tee ~/.config/polybar/polybar-scripts/iface >/dev/null
+	sleep 30s && ip link | awk '/UP mode/ {print $2}' | tr -d ':' | tee ~/.config/polybar/polybar-scripts/iface >/dev/null
 else
 	iface=$(cat ~/.config/polybar/polybar-scripts/iface)
 	doas macchanger -e $iface >/dev/null
