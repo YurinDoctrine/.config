f5b81bdb0d3d07aef31fbdedc0b9602a5089d302
Yurin Doctrine <doctriney@protonmail.com>
2024-06-24 20:19:31 +0300

    edit polybar/polybar-scripts/usb.sh

--- polybar/polybar-scripts/usb.sh
+++ polybar/polybar-scripts/usb.sh
@@ -4,7 +4,7 @@ usb_print() {
     devices=$(lsblk -Jplno NAME,TYPE,RM,SIZE,MOUNTPOINT,VENDOR)
     output=""
 
-    for unmounted in $(echo -e "$devices" | jq -r '.blockdevices[] | select(.type == "part") | select(.rm == true) | select(.mountpoint == null) | .name'); do
+    for unmounted in $(echo -e "$devices" | jq -r '.blockdevices[] | select(.rm == true) | select(.mountpoint == null) | .name'); do
         unmounted=$(echo -e "$unmounted" | tr -d "[:digit:]")
         unmounted=$(echo -e "$devices" | jq -r '.blockdevices[] | select(.name == "'"$unmounted"'") | .vendor')
         unmounted=$(echo -e "$unmounted" | tr -d ' ')
@@ -12,7 +12,7 @@ usb_print() {
         output="\uf052"
     done
 
-    for mounted in $(echo -e "$devices" | jq -r '.blockdevices[] | select(.type == "part") | select(.rm == true) | select(.mountpoint != null) | .size'); do
+    for mounted in $(echo -e "$devices" | jq -r '.blockdevices[] | select(.rm == true) | select(.mountpoint != null) | .size'); do
 
         output="\uf052"
     done
@@ -24,7 +24,7 @@ case "$1" in
     --eject)
         devices=$(lsblk -Jplno NAME,TYPE,RM,MOUNTPOINT)
 
-        for unmount in $(echo "$devices" | jq -r '.blockdevices[] | select(.type == "part") | select(.rm == true) | select(.mountpoint != null) | .name'); do
+        for unmount in $(echo "$devices" | jq -r '.blockdevices[] | select(.rm == true) | select(.mountpoint != null) | .name'); do
             udisksctl unmount --no-user-interaction -b "$unmount"
             udisksctl power-off --no-user-interaction -b "$unmount"
         done
