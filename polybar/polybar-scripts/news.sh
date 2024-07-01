5e1bc5b4b5f2fac658b611c5241b27e407dae170
Yurin Doctrine <doctriney@protonmail.com>
2024-07-01 17:18:50 +0300

    edit polybar/polybar-scripts/news.sh

--- polybar/polybar-scripts/news.sh
+++ polybar/polybar-scripts/news.sh
@@ -1,40 +1,11 @@
-#!/usr/bin/sh
+#!/usr/bin/env bash
 
-start_newsboat() {
-  exec kitty newsboat -r
-}
+counter=1
+while [ $counter -le 5 ]
+do
+	news_title=$(curl -s https://www.cnbc.com/id/100727362/device/rss/rss.html | xmlstarlet sel -t -m '//item' -v 'title' -n | head -n5 | awk 'NR=='$counter'')
+	news_description=$(curl -s https://www.cnbc.com/id/100727362/device/rss/rss.html | xmlstarlet sel -t -m '//item' -v 'description' -n | head -n5 | awk 'NR=='$counter'')
 
-case "$1" in
-    --start_newsboat)
-        start_newsboat
-        ;;
-    *)
-        main() {
-            counter=1
-            while [ $counter -le 5 ]
-            do
-                news_title=$(curl -s https://www.cnbc.com/id/100727362/device/rss/rss.html | xmlstarlet sel -t -m '//item' -v 'title' -n | head -n5 | awk 'NR=='$counter'')
-                news_title="${news_title} . "
-                title_lenght=${#news_title}
-                lenght_count=(${title_lenght}*3)
-                display_width=32
-                if [ $title_lenght -gt $display_width ]; then
-                    while [ $lenght_count -gt 0 ]
-                    do
-                        display_title="${news_title:0:$display_width}"
-                        echo -e $display_title
-                        sleep 0.5
-                        news_title="${news_title:1}${news_title:0:1}"
-                        ((lenght_count--))
-                    done
-                else
-                    echo -e $news_title
-                    sleep 60s
-                fi
-                ((counter++))
-	    done
-            echo ''
-        }
-        main
-        ;;
-esac
+	notify-send -t 0 "$news_title" "$news_description" -u normal -i notification-message-im
+	((counter++))
+done
