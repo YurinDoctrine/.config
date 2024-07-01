ec91b9b6b056f0097ce79045d0cbbc1c561c490c
Yurin Doctrine <doctriney@protonmail.com>
2024-07-01 17:40:50 +0300

    edit polybar/polybar-scripts/news.sh

--- polybar/polybar-scripts/news.sh
+++ polybar/polybar-scripts/news.sh
@@ -7,5 +7,6 @@ do
 	news_description=$(curl -s https://www.cnbc.com/id/100727362/device/rss/rss.html | xmlstarlet sel -t -m '//item' -v 'description' -n | head -n5 | awk 'NR=='$counter'')
 
 	notify-send -t 0 "$news_title" "$news_description" -u normal -i notification-message-im
+	echo ""
 	((counter++))
 done
