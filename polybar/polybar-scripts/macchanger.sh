1fbf6b50afca3e43471338de0932fbec6ebdcc7f
Yurin Doctrine <doctriney@protonmail.com>
2024-07-10 20:30:45 +0300

    edit polybar/polybar-scripts/macchanger.sh

--- polybar/polybar-scripts/macchanger.sh
+++ polybar/polybar-scripts/macchanger.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/sh
 
+blocked_count=$(rfkill list | grep -c "blocked: yes")
+
+if [ "$blocked_count" -gt 2 ]; then
+	echo "\uf072"
+else
+	echo ''
+fi
+
 if [[ $(ip link | grep 'UP mode' | wc -l) -gt 0 ]]; then
 	ip link | awk '/UP mode/ {print $2}' | tr -d ':' | tee ~/.config/polybar/polybar-scripts/iface >/dev/null
 else
