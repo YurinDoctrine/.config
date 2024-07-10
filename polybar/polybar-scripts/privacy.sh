75eb6d037d598f7696146b486e5d67c38d502901
YurinDoctrine <doctriney@protonmail.com>
2024-07-10 18:17:37 +0300

    add polybar/polybar-scripts/privacy.sh

---
+++ polybar/polybar-scripts/privacy.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+MODULES=("snd_hda_intel" "snd_usb_audio" "uvcvideo" "gspca_main")
+
+is_module_loaded() {
+    lsmod | grep -q "^${1//-/_}\b"
+}
+
+print_status() {
+    for module in "${MODULES[@]}"; do
+        if is_module_loaded "$module"; then
+            echo -e "\uf06e"
+			break
+        else
+            echo -e "\uf070"
+        fi
+    done
+}
+
+toggle_modules() {
+    for module in "${MODULES[@]}"; do
+        if is_module_loaded "$module"; then
+            doas rmmod -f "$module" >/dev/null
+        else
+            doas modprobe -f "$module" >/dev/null
+        fi
+    done
+}
+
+case "$1" in
+	--toggle)
+		toggle_modules
+		;;
+	*)
+		print_status
+		;;
+esac
