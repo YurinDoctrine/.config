e33a2d72244cd10fe0f612690bf355da9c46ac30
Yurin Doctrine <doctriney@protonmail.com>
2024-09-17 00:58:10 +0300

    add .irssi/scripts/autorun/notify.pl

---
+++ .irssi/scripts/autorun/notify.pl
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+use Irssi;
+
+sub sig_message_public {
+    my ($server, $msg, $nick, $address, $target) = @_;
+
+    my $notification_msg = "$nick from $target: $msg";
+
+    system("notify-send '$notification_msg' -u normal -i notification-message-im");
+}
+
+Irssi::signal_add('message public', 'sig_message_public');
