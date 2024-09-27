192b595aad6cc3ebdf096ab0c2eaedb01b150834
Yurin Doctrine <doctriney@protonmail.com>
2024-09-28 00:28:13 +0300

    edit polybar/polybar-scripts/news.sh

--- polybar/polybar-scripts/news.sh
+++ polybar/polybar-scripts/news.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+news_feed=$(curl -s https://www.cnbc.com/id/100727362/device/rss/rss.html)
+
 counter=1
+
 while [ $counter -le 5 ]
 do
-	news_title=$(curl -s https://www.cnbc.com/id/100727362/device/rss/rss.html | xmlstarlet sel -t -m '//item' -v 'title' -n | head -n5 | awk 'NR=='$counter'')
-	news_description=$(curl -s https://www.cnbc.com/id/100727362/device/rss/rss.html | xmlstarlet sel -t -m '//item' -v 'description' -n | head -n5 | awk 'NR=='$counter'')
+	news_title=$(echo "$news_feed" | xmlstarlet sel -t -m '//item' -v 'title' -n | awk 'NR=='$counter'')
+	news_description=$(echo "$news_feed" | xmlstarlet sel -t -m '//item' -v 'description' -n | awk 'NR=='$counter'')
+	news_link=$(echo "$news_feed" | xmlstarlet sel -t -m '//item' -v 'link' -n | awk 'NR=='$counter'')
 
-	notify-send -t 0 "$news_title" "$news_description" -u normal -i notification-message-im
-	echo ''
+	notify=$(notify-send -t 0 -u normal -i notification-message-im -A "open=Open URL" "$news_title" "$news_description") && [[ $notify == "open" ]] && xdg-open "$news_link" &
+
+	echo ""
+
 	((counter++))
 done
