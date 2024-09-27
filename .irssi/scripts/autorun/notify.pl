d3547eea0d34867c856089bbf07610bf6fd417bc
Yurin Doctrine <doctriney@protonmail.com>
2024-09-28 01:41:27 +0300

    edit .irssi/scripts/autorun/notify.pl

--- .irssi/scripts/autorun/notify.pl
+++ .irssi/scripts/autorun/notify.pl
@@ -7,13 +7,13 @@ sub sig_message_public {
 
     my $notification_msg = "[$target] $nick: $msg";
 
-    system("notify-send '$notification_msg' -u normal -i notification-message-im");
+    system("notify=\$(notify-send -u normal -i notification-message-im -A 'open=Open IRC' '$notification_msg') && [[ \$notify == 'open' ]] && [[ \$(tmux list-sessions -F '#{session_name} #{session_attached}' | grep '^irc 0\$') ]] && exec xterm -e tmux attach -t irc >/dev/null 2>&1 &");
 }
 
 sub sig_message_private {
     my ($server, $msg, $nick, $address) = @_;
 
-    system("notify-send '$nick' '$msg' -u critical -i notification-message-im");
+    system("notify=\$(notify-send -u critical -i notification-message-im -A 'open=Open IRC' '$nick' '$msg') && [[ \$notify == 'open' ]] && [[ \$(tmux list-sessions -F '#{session_name} #{session_attached}' | grep '^irc 0\$') ]] && exec xterm -e tmux attach -t irc >/dev/null 2>&1 &");
 }
 
 Irssi::signal_add('message public', 'sig_message_public');
