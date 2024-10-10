242177546e927e7d67953e48714aabf851b9fb4b
Yurin Doctrine <doctriney@protonmail.com>
2024-10-10 23:22:22 +0300

    edit polybar/polybar-scripts/notify.sh

--- polybar/polybar-scripts/notify.sh
+++ polybar/polybar-scripts/notify.sh
@@ -3,12 +3,27 @@
 case "$1" in
     --toggle)
         dunstctl set-paused toggle
-        dunstctl history-pop
+        if [[ ! -e "$HOME/.config/polybar/polybar-scripts/dnd" ]]; then
+            touch "$HOME/.config/polybar/polybar-scripts/dnd"
+        else
+            rm "$HOME/.config/polybar/polybar-scripts/dnd"
+        fi
         ;;
     *)
         if [ "$(dunstctl is-paused)" = "true" ]; then
+            if [[ ! -e "$HOME/.config/polybar/polybar-scripts/dnd" ]]; then
+                xdotool getwindowgeometry "$(xdotool getactivewindow)" | \
+                grep -q "$(xdpyinfo | grep dimensions | awk '{print $2}')" || \
+                dunstctl set-paused toggle
+            fi
+            dunstctl history-pop
             echo "\uf0a2"
         else
+            if [[ ! -e "$HOME/.config/polybar/polybar-scripts/dnd" ]]; then
+                xdotool getwindowgeometry "$(xdotool getactivewindow)" | \
+                grep -q "$(xdpyinfo | grep dimensions | awk '{print $2}')" && \
+                dunstctl set-paused toggle
+            fi
             echo "\uf0f3"
         fi
         ;;
