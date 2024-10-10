badee0501cbe7802df39169259f86d9316e0e03f
Yurin Doctrine <doctriney@protonmail.com>
2024-10-10 19:06:41 +0300

    edit .irssi/scripts/autorun/notify.pl

--- .irssi/scripts/autorun/notify.pl
+++ .irssi/scripts/autorun/notify.pl
@@ -13,7 +13,7 @@ sub sig_message_public {
 sub sig_message_private {
     my ($server, $msg, $nick, $address) = @_;
 
-    system("xdotool getwindowgeometry \$(xdotool getactivewindow) | grep -q \$(xdpyinfo | grep dimensions | awk '{print \$2}') || notify=\$(notify-send -u critical -i notification-message-im -A 'open=Open IRC' '$nick' '$msg') && [[ \$notify == 'open' ]] && [[ \$(tmux list-sessions -F '#{session_name} #{session_attached}' | grep '^irc 0\$') ]] && exec xterm -e tmux attach -t irc >/dev/null 2>&1 &");
+    system("notify=\$(notify-send -u critical -i notification-message-im -A 'open=Open IRC' '$nick' '$msg') && [[ \$notify == 'open' ]] && [[ \$(tmux list-sessions -F '#{session_name} #{session_attached}' | grep '^irc 0\$') ]] && exec xterm -e tmux attach -t irc >/dev/null 2>&1 &");
 }
 
 Irssi::signal_add('message public', 'sig_message_public');
