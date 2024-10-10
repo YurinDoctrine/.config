d88f9c2da95afd525603b66dcf75dbd02af2105a
Yurin Doctrine <doctriney@protonmail.com>
2024-10-11 00:05:53 +0300

    edit polybar/polybar-scripts/macchanger.sh

--- polybar/polybar-scripts/macchanger.sh
+++ polybar/polybar-scripts/macchanger.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/sh
 
 if [[ $(ip link | grep 'UP mode' | wc -l) -gt 0 ]]; then
-	ip link | awk '/UP mode/ {print $2}' | tr -d ':' | tee ~/.config/polybar/polybar-scripts/iface >/dev/null
+	ip link | awk '/UP mode/ {print $2}' | tr -d ':' | tee ~/.config/polybar/polybar-scripts/iface >/dev/null 2>&1
 else
 	IFACE=$(cat ~/.config/polybar/polybar-scripts/iface)
 	if [ "$IFACE" ]; then
-		doas macchanger -a $IFACE >/dev/null
-		doas sv restart wpa_supplicant >/dev/null && doas sv restart dhcpcd >/dev/null
-		rm ~/.config/polybar/polybar-scripts/iface >/dev/null
+		doas macchanger -a $IFACE >/dev/null 2>&1
+		doas sv restart wpa_supplicant >/dev/null 2>&1 && doas sv restart dhcpcd >/dev/null 2>&1
+		rm ~/.config/polybar/polybar-scripts/iface >/dev/null 2>&1
 	fi
 fi
 
